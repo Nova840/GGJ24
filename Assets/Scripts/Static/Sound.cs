f92d375e04e1df18f6ee6600aa05165a664b5144
Nova840 <adamzmudzinski2@gmail.com>
2024-02-12 20:26:03 -0500

    Fix sound pitch variation
    Random pitch variation had a bias toward lower pitches

--- Assets/Scripts/Static/Sound.cs
+++ Assets/Scripts/Static/Sound.cs
@@ -13,7 +13,7 @@ public class Sound {
     [Range(0, 1)]
     public float volume = 1;
 
-    [Range(0, .99f)]
+    [Range(0, 3)]
     public float randomPitchVariation = 0;
 
     public static void Play(Sound[] sounds) => Play(sounds[Random.Range(0, sounds.Length)]);
@@ -22,7 +22,7 @@ public class Sound {
         AudioSource source = new GameObject("Sound: " + clip.name).AddComponent<AudioSource>();
         source.clip = clip;
         source.volume = volume;
-        source.pitch = 1 + Random.Range(-randomPitchVariation, randomPitchVariation);
+        source.pitch = Random.Range(1 / (randomPitchVariation + 1), randomPitchVariation + 1);
         source.Play();
         Object.Destroy(source.gameObject, clip.length / source.pitch);
     }
