42777a56f67c9692a1c1684f95bcf1cb92d9a019
Nova840 <adamzmudzinski2@gmail.com>
2024-02-22 02:02:30 -0500

    New camera movement

--- Assets/Scripts/CameraManager.cs
+++ Assets/Scripts/CameraManager.cs
@@ -1,31 +1,65 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class CameraManager : MonoBehaviour {
 
     [SerializeField]
-    private float moveSpeed;
+    private float moveSmoothingSpeed, zoomSmoothingSpeed;
+
+    [SerializeField]
+    private AnimationCurve cameraDistanceAtBoundsSize;
+
+    [SerializeField]
+    private float playerTrackingHeight;
+
+    private Camera _camera;
 
     private List<Player> players = new List<Player>();
     public void AddPlayerTransform(Player player) => players.Add(player);
 
-    private void Update() {
-        int count = players.Count;
-        if (count == 0) return;
+    private void Awake() {
+        _camera = GetComponentInChildren<Camera>();
+    }
 
-        Vector3 averagePos = Vector3.zero;
-        foreach (Player player in players) {
-            if (player.PlayerMove.Respawning) {
-                count--;
-                if (count == 0) return;
-            } else {
-                averagePos += player.transform.position;
-            }
+    private void Start() {
+        if (players.Count == 0) return;
+        Bounds bounds = GetRelativeBounds();
+        transform.position = GetTargetPosition(bounds);
+        _camera.transform.localPosition = new Vector3(0, 0, -GetTargetDistance(bounds));
+    }
+
+    private void LateUpdate() {
+        if (players.Count == 0) return;
+        Bounds bounds = GetRelativeBounds();
+        transform.position = Vector3.Lerp(transform.position, GetTargetPosition(bounds), moveSmoothingSpeed * Time.deltaTime);
+        _camera.transform.localPosition = new Vector3(0, 0, Mathf.Lerp(_camera.transform.localPosition.z, -GetTargetDistance(bounds), zoomSmoothingSpeed * Time.deltaTime));
+    }
+
+    private Bounds GetRelativeBounds() {
+        Vector3 GetPlayerRelativePosition(Player player) {
+            Vector3 position = player.transform.position;
+            position.y = playerTrackingHeight;
+            return transform.InverseTransformPoint(position);
         }
 
-        averagePos /= count;
-        transform.position = Vector3.Lerp(transform.position, averagePos, moveSpeed * Time.deltaTime);
+        Bounds bounds = new Bounds(GetPlayerRelativePosition(players[0]), Vector3.zero);
+        for (int i = 1; i < players.Count; i++) {
+            Vector3 relativePos = GetPlayerRelativePosition(players[i]);
+            bounds.Encapsulate(relativePos);
+        }
+        return bounds;
+    }
+
+    private Vector3 GetTargetPosition(Bounds bounds) {
+        Vector3 center = transform.TransformPoint(bounds.center);
+        return center + transform.rotation * Vector3.back * bounds.extents.z;
+    }
+
+    private float GetTargetDistance(Bounds bounds) {
+        float size = Mathf.Max(bounds.size.x, bounds.size.y);
+        return cameraDistanceAtBoundsSize.Evaluate(size);
     }
 
 }
